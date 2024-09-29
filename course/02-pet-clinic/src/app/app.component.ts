06baca2ec2228fcd50c4b726cf1b11388726f032
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-09-29 13:18:40 -0600

    :sparkles: Finish pat clinic project

--- course/02-pet-clinic/src/app/app.component.ts
+++ course/02-pet-clinic/src/app/app.component.ts
@@ -1,13 +1,85 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PatientComponent } from './components/patient/patient.component';
+import { PatientService } from './services/patient.service';
+import { Patient } from './types/patient.type';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, PatientComponent, FormComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = '02-pet-clinic';
+export class AppComponent implements OnInit {
+  patients: Patient[] = [];
+  patient: Patient = {
+    id: '',
+    name: '',
+    property: '',
+    email: '',
+    date: '',
+    symptoms: ''
+  };
+
+  constructor(private _patientService: PatientService) { }
+
+  ngOnInit(): void {
+    this.patients = this._patientService.getPatients();
+  }
+
+  onChangeName(name: string) {
+    this.patient.name = name;
+  }
+
+  onChangeProperty(property: string) {
+    this.patient.property = property;
+  }
+
+  onChangeEmail(email: string) {
+    this.patient.email = email;
+  }
+
+  onChangeDate(date: string) {
+    this.patient.date = date;
+  }
+
+  onChangeSymptoms(symptoms: string) {
+    this.patient.symptoms = symptoms;
+  }
+
+  savePatient() {
+    if (this.patient.id) {
+      this._patientService.updatePatient(this.patient, this.patients);
+    } else {
+      this._patientService.addPatient(this.patient, this.patients);
+    }
+
+    this.resetForm();
+  }
+
+  updatePatient(id: string) {
+    const patientToEdit = this.patients.find((p) => p.id === id);
+
+    if (patientToEdit) {
+      this.patient = { ...patientToEdit };
+    }
+  }
+
+  deletePatient(id: string) {
+    this.patients = this._patientService.deletePatient(id, this.patients);
+  }
+
+  resetForm() {
+    this.patient = {
+      id: null,
+      name: '',
+      property: '',
+      email: '',
+      date: '',
+      symptoms: '',
+    };
+  }
 }
