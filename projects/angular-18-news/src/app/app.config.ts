95c4d0905108221d6679867b6632cff240b29085
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-12-21 18:26:29 -0600

    :sparkles: Init config and routes

---
+++ projects/angular-18-news/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideClientHydration(),
+    provideCharts(withDefaultRegisterables())
+  ]
+};
