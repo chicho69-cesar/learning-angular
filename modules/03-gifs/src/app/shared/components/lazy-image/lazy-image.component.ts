7c74c89040a756c72d0ec1d9bab6a93a4130b235
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-26 19:30:34 -0600

    :sparkles: Add shared module components

---
+++ modules/03-gifs/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  standalone: false,
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  public ngOnInit(): void {
+    if (!this.url) {
+      throw new Error('url is required');
+    }
+  }
+
+  public onLoad(): void {
+    this.hasLoaded = true;
+  }
+}
---
+++ modules/03-gifs/src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    LazyImageComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LazyImageComponent,
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
