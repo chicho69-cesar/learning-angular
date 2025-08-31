c4812af286030fc9692777879fff2a21ed831ec6
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-30 23:54:49 -0600

    :sparkles: Add shared module components

---
+++ modules/05-pipes-app/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  standalone: false,
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [];
+
+  public ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers'
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'uncommon'
+          },
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Custom Pipes',
+            icon: 'pi pi-cog',
+            routerLink: 'custom'
+          }
+        ]
+      }
+    ];
+  }
+}
