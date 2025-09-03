938c65b7ffb2a8860f13f309e13bacf448365d5c
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-09-03 09:52:13 -0600

    :sparkles: Add custom validators

---
+++ modules/07-reactive-forms/src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { delay, Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EmailValidatorService implements AsyncValidator {
+  public validate(control: AbstractControl): Observable<ValidationErrors | null> {
+    const email = control.value;
+
+    const httpCallObservable$ = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({ email });
+
+      if (email === 'cesar@google.com') {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe(
+      delay(3000),
+    );
+
+    return httpCallObservable$;
+  }
+}
