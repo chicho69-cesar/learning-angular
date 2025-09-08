23b5e48b6adee3e0e82e3c0a00da7be3f5fb3c88
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-09-08 00:16:28 -0600

    :sparkles: Add signals components and services

---
+++ modules/11-signal-directives/src/app/signals/layouts/signals-layout/signals-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signals-signals-layout',
+  standalone: false,
+  templateUrl: './signals-layout.component.html',
+  styleUrl: './signals-layout.component.css'
+})
+export class SignalsLayoutComponent {
+
+}
