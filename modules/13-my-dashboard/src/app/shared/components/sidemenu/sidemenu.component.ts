66a021eb1f0c5e7aa689da17677ec7fce9010d75
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2025-09-10 23:42:41 -0600

    :sparkles: Add shared components

---
+++ modules/13-my-dashboard/src/app/shared/components/sidemenu/sidemenu.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../../app.routes';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css',
+})
+export default class SidemenuComponent {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
+
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map((route) => route.children ?? [])
+    //   .flat()
+    //   .filter((route) => route && route.path)
+    //   .filter((route) => !route.path?.includes(':'))
+    // console.log(dashboardRoutes);
+  }
+}
