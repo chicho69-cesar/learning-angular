2238148198bee2890b0b89e62dc94b949c82adb6
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-09-13 19:03:28 -0600

    :sparkles: Finish teslo server

---
+++ standalone/09-teslo-server/src/app.module.ts
@@ -0,0 +1,47 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'node:path';
+
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { FilesModule } from './files/files.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
+import { ProductsModule } from './products/products.module';
+import { SeedModule } from './seed/seed.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    CommonModule,
+    ProductsModule,
+    SeedModule,
+    FilesModule,
+    AuthModule,
+    MessagesWsModule,
+  ],
+})
+export class AppModule { }
