3d380e2eef505e2acf5d1f06edf4eb1124c5dc81
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-09-13 19:01:47 -0600

    :sparkles: Add files module

---
+++ standalone/09-teslo-server/src/files/files.service.ts
@@ -0,0 +1,15 @@
+import { existsSync } from 'node:fs';
+import { join } from 'node:path';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FilesService {
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+
+    if (!existsSync(path))
+      throw new BadRequestException(`No product found with image ${imageName}`);
+
+    return path;
+  }
+}
