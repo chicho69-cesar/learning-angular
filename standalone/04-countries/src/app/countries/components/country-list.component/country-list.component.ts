643ccdcc84c58be2cc5f8fae55ad17f5c2fc6722
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-12-15 12:37:39 -0600

    :art: Init all files to work on countries app

---
+++ standalone/04-countries/src/app/countries/components/country-list.component/country-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-list.component',
+  imports: [],
+  templateUrl: './country-list.component.html',
+  styleUrl: './country-list.component.css'
+})
+export class CountryListComponent {
+
+}
---
+++ standalone/04-countries/src/app/countries/layouts/country-layout.component/country-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-layout.component',
+  imports: [],
+  templateUrl: './country-layout.component.html',
+  styleUrl: './country-layout.component.css'
+})
+export class CountryLayoutComponent {
+
+}
