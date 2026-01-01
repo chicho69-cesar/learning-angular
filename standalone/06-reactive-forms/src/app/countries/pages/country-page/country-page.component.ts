4a5511d2e1d78e4da6c8e0674be1cb5c90299010
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-01 10:50:12 -0600

    :art: Init reactive forms files and components

---
+++ standalone/06-reactive-forms/src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'countries-country-page',
+  imports: [],
+  templateUrl: './country-page.component.html',
+  styleUrl: './country-page.component.css'
+})
+export class CountryPageComponent {
+
+}
---
+++ standalone/06-reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'reactive-switches-page',
+  imports: [],
+  templateUrl: './switches-page.component.html',
+  styleUrl: './switches-page.component.css'
+})
+export class SwitchesPageComponent {
+
+}
