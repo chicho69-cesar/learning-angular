c58b9f9c1c27d24ed2a73c0c929c0269c69e1b7d
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-01 11:13:41 -0600

    :art: Init maps files and components

---
+++ standalone/08-maps/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FullscreenMapPageComponent } from './fullscreen-map-page.component';
+
+describe('FullscreenMapPageComponent', () => {
+  let component: FullscreenMapPageComponent;
+  let fixture: ComponentFixture<FullscreenMapPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FullscreenMapPageComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FullscreenMapPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
