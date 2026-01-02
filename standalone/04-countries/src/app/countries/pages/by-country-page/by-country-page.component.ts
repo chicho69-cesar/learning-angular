1b81af8dc4c2bb4b2a7b5dfeace19f1fe02b6e58
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-01 18:15:16 -0600

    :sparkles: Add by country page

--- standalone/04-countries/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ standalone/04-countries/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, linkedSignal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-country-page',
-  imports: [],
+  imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-country-page.component.html',
   styleUrl: './by-country-page.component.css'
 })
 export class ByCountryPageComponent {
+  public countryService = inject(CountryService);
+  public activatedRoute = inject(ActivatedRoute);
+  public router = inject(Router);
 
+  public queryParam = this.activatedRoute.snapshot.paramMap.get('query') || '';
+
+  public query = linkedSignal(() => this.queryParam);
+
+  public countryResource = rxResource({
+    params: () => ({ query: this.query() }),
+    stream: ({ params }) => {
+      if (!params.query) return of([]);
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: params.query,
+        },
+      });
+
+      return this.countryService.searchByCountry(params.query);
+    },
+  });
 }
