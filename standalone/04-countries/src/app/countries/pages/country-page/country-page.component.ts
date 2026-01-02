a304588d69bbfcac759eac3ae292cbb7cbfebfdb
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-01 18:14:52 -0600

    :sparkles: Add country page

--- standalone/04-countries/src/app/countries/pages/country-page/country-page.component.ts
+++ standalone/04-countries/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+
+import { NotFoundPageComponent } from '../../../shared/pages/not-found-page/not-found-page.component';
+import { CountryInformationComponent } from '../../components/country-information/country-information.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [NotFoundPageComponent, CountryInformationComponent],
   templateUrl: './country-page.component.html',
   styleUrl: './country-page.component.css'
 })
 export class CountryPageComponent {
+  public countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  public countryService = inject(CountryService);
 
+  public countryResource = rxResource({
+    params: () => ({ code: this.countryCode }),
+    stream: ({ params }) => this.countryService.searchByAlphaCode(params.code),
+  });
 }
