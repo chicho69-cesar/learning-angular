41676f9d99bb83d52659889408fb0c58e525b08c
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-01 18:13:30 -0600

    :art: Add country layout

--- standalone/04-countries/src/app/countries/layouts/country-layout/country-layout.component.ts
+++ standalone/04-countries/src/app/countries/layouts/country-layout/country-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { TopMenuComponent } from '../../components/top-menu/top-menu.component';
 
 @Component({
   selector: 'app-country-layout',
-  imports: [],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './country-layout.component.html',
   styleUrl: './country-layout.component.css'
 })
