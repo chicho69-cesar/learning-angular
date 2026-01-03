1297d3395f2f928aa0df62375ed2d937f2ec0173
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:26:54 -0600

    :sparkles: Add basic pipes page

--- standalone/05-pipes/src/app/pages/basic-page/basic-page.component.ts
+++ standalone/05-pipes/src/app/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css'
 })
-export class BasicPageComponent {
+export default class BasicPageComponent {
+  public localeService = inject(LocaleService);
+  public currentLocale = signal(inject(LOCALE_ID));
 
+  public nameLower = signal('cesar');
+  public nameUpper = signal('CESAR');
+  public fullName = signal('CeSar ViLLaLobOS OlMoS');
+
+  public customDate = signal(new Date());
+
+  public tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('Tick');
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
+
+  public changeLocale(locale: AvailableLocale) {
+    console.log('Changing locale to:', locale);
+    this.localeService.changeLocale(locale);
+  }
 }
