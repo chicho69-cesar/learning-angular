bf844504f9b4a7c62e67158b3c0f394b2f755f14
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:36:25 -0600

    :art: Add shared components for reactive forms

--- standalone/06-reactive-forms/src/app/shared/components/side-menu/side-menu.component.ts
+++ standalone/06-reactive-forms/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { REACTIVE_ROUTES } from '../../../reactive/reactive.routes';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = REACTIVE_ROUTES[0].children ?? [];
 
 @Component({
   selector: 'shared-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+  public reactiveMenu: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((item) => ({
+      route: `reactive/${item.path}`,
+      title: `${item.title}`
+    }))
 
+  public authMenu: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  public countryMenu: MenuItem[] = [
+    {
+      title: 'Países',
+      route: './country',
+    },
+  ];
 }
