5bb0d0adeed617a184ef76ce7f43ee78642518fa
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:36:01 -0600

    :sparkles: Finish life cycle project

--- standalone/07-life-cycle/src/app/app.component.ts
+++ standalone/07-life-cycle/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
-  protected readonly title = signal('07-life-cycle');
-}
+export class App { }
--- standalone/07-life-cycle/src/app/app.config.ts
+++ standalone/07-life-cycle/src/app/app.config.ts
@@ -1,12 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
   ]
 };
