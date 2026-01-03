5549f41f5b044a054e015c9edab2dcfe86aee05e
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:26:40 -0600

    :art: Add components for pipes

--- standalone/05-pipes/src/app/components/navbar/navbar.component.ts
+++ standalone/05-pipes/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  public routes = routes.map((route) => ({
+    title: route.title ?? '',
+    path: route.path ?? '',
+  }));
 }
