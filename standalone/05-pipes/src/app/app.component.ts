bc475c306a8e4cac428fb20d3423e2cef1787203
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:31:50 -0600

    :sparkles: Finish pipes project

--- standalone/05-pipes/src/app/app.component.ts
+++ standalone/05-pipes/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class App {
-  protected readonly title = signal('05-pipes');
-}
+export class App { }
