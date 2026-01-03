29070afe8ace207a8b8edca8d4d945a3671cea3b
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-01-02 19:32:34 -0600

    :art: Add life cycle components

--- standalone/07-life-cycle/src/app/components/title/title.component.ts
+++ standalone/07-life-cycle/src/app/components/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   templateUrl: './title.component.html',
   styleUrl: './title.component.css'
 })
-export class TitleComponent {
+export class TitleComponent implements OnChanges {
+  public title = input.required<string>();
+  public title1 = input.required<string>();
+  public title2 = input.required<string>();
+  public title3 = input.required<string>();
+  public title4 = input.required<string>();
 
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called in TitleComponent');
+
+    for (let change in changes) {
+      const value = changes[change];
+
+      console.log(`Property '${change}' changed:`);
+      console.log(`Previous ${change} == ${value.previousValue}`);
+      console.log(`Current ${change} == ${value.currentValue}`);
+      console.log(`Is first ${change} change == ${value.firstChange}`);
+    }
+  }
 }
