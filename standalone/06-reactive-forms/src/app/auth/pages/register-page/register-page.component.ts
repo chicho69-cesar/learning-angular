07d6a9fe6860904132b52fffc2435cd22688c265
Cesar <cesarvillalobosolmos.01@gmail.com>
2026-01-03 23:19:13 -0600

    :sparkles: Add register page for reactive forms

--- standalone/06-reactive-forms/src/app/auth/pages/register-page/register-page.component.ts
+++ standalone/06-reactive-forms/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormUtils } from '../../../utils/form.utils';
 
 @Component({
   selector: 'auth-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  public fb = inject(FormBuilder);
+  public formUtils = FormUtils;
 
+  public authForm = this.fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.namePattern)],
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(FormUtils.emailPattern)],
+        [FormUtils.checkingServerResponse],
+      ],
+      username: [
+        '',
+        [Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern), FormUtils.notStrider],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(6)],
+      ],
+      confirmPassword: [
+        '',
+        Validators.required,
+      ],
+    },
+    {
+      validators: [FormUtils.isFieldOneEqualFieldTwo('password', 'confirmPassword')],
+    }
+  );
+
+  public onSubmit(): void {
+    this.authForm.markAllAsTouched();
+    console.log(this.authForm.value);
+  }
 }
