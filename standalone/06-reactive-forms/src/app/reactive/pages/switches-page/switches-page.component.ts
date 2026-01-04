8fe98ffa71a23879599a4b5a0bf57bb22111f4a4
Cesar <cesarvillalobosolmos.01@gmail.com>
2026-01-03 23:18:52 -0600

    :sparkles: Add switches forms page

--- standalone/06-reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ standalone/06-reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormUtils } from '../../../utils/form.utils';
 
 @Component({
   selector: 'reactive-switches-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './switches-page.component.html',
   styleUrl: './switches-page.component.css'
 })
 export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  public formUtils = FormUtils;
 
+  public myForm = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
+
+  public onSubmit(): void {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
 }
