eff33e2de95a3bf77fab0f76f61461621d97a4d9
Cesar <cesarvillalobosolmos.01@gmail.com>
2026-01-03 23:18:10 -0600

    :sparkles: Add country types and services

--- standalone/06-reactive-forms/src/app/countries/services/country.service.ts
+++ standalone/06-reactive-forms/src/app/countries/services/country.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { combineLatest, Observable, of } from 'rxjs';
+
+import { Country } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
-  
+  private baseUrl = 'https://restcountries.com/v3.1';
+  private http = inject(HttpClient);
+
+  private _regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public get regions(): string[] {
+    return [...this._regions];
+  }
+
+  public getCountriesByRegion(region: string): Observable<Country[]> {
+    if (!region) return of([]);
+    const url = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.http.get<Country[]>(url);
+  }
+
+  public getCountryByAlphaCode(code: string): Observable<Country> {
+    const url = `${this.baseUrl}/alpha/${code}?fields=cca3,name,borders`;
+    return this.http.get<Country>(url);
+  }
+
+  public getCountryNamesByCodeArray(countryCodes: string[]): Observable<Country[]> {
+    if (!countryCodes || countryCodes.length === 0) return of([]);
+
+    const countriesRequests: Observable<Country>[] = [];
+
+    countryCodes.forEach((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequests.push(request);
+    });
+
+    return combineLatest(countriesRequests);
+  }
 }
