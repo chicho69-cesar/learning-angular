c8ab0ec956c16f6ca7996857a7f1ea27cf266b0a
Cesar <cesarvillalobosolmos.01@gmail.com>
2026-01-03 23:17:53 -0600

    :sparkles: Add form utils for reactive forms

---
+++ standalone/06-reactive-forms/src/app/utils/form.utils.ts
@@ -0,0 +1,103 @@
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+
+async function sleep(ms: number = 2500): Promise<boolean> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    }, ms);
+  })
+}
+
+export class FormUtils {
+  public static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  public static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
+  public static getTextError(errors: ValidationErrors) {
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+         case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}`;
+
+        case 'email':
+          return `El valor ingresado no es un correo electrónico`;
+
+        case 'emailTaken':
+          return `El correo electrónico ya está siendo usado por otro usuario`;
+
+        case 'noStrider':
+          return `No se puede usar el username de strider en la app`;
+
+        case 'pattern':
+          if (errors['pattern'].requiredPattern === FormUtils.emailPattern) {
+            return 'El valor ingresado no luce como un correo electrónico';
+          }
+
+          return 'Error de patrón contra expresión regular';
+
+        default:
+          return `Error de validación no controlado ${key}`;
+      }
+    }
+
+    return null;
+  }
+
+  public static isValidField(form: FormGroup, fieldName: string): boolean | null {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    );
+  }
+
+  public static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
+    const errors = form.controls[fieldName].errors ?? {};
+    return FormUtils.getTextError(errors);
+  }
+
+  public static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  public static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+    if (formArray.controls.length === 0) return null;
+    const errors = formArray.controls[index].errors ?? {};
+    return FormUtils.getTextError(errors);
+  }
+
+  public static isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { passwordsNotEqual: true };
+    };
+  }
+
+  public static async checkingServerResponse(control: AbstractControl): Promise<ValidationErrors | null> {
+    await sleep();
+
+    const formValue = control.value;
+
+    if (formValue === 'hola@mundo.com') {
+      return {
+        emailTaken: true,
+      };
+    }
+
+    return null;
+  }
+
+  public static notStrider(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    return value === 'strider' ? { noStrider: true } : null;
+  }
+}
