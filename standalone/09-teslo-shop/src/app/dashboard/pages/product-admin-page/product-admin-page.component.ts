f9be61be1db284ec190a44bd3c06e712e60cb1cd
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-04-01 19:53:42 -0600

    :art: Add admin pages

--- standalone/09-teslo-shop/src/app/dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ standalone/09-teslo-shop/src/app/dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+
+import { ProductService } from '../../../products/services/product.service';
+import { ProductDetailsComponent } from "../../components/product-details/product-details.component";
 
 @Component({
   selector: 'dashboard-product-admin-page',
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './product-admin-page.component.html',
   styleUrl: './product-admin-page.component.css'
 })
 export class ProductAdminPageComponent {
+  public activatedRoute = inject(ActivatedRoute);
+  public router = inject(Router);
+  public productsService = inject(ProductService);
 
+  public productId = toSignal(
+    this.activatedRoute.params.pipe(map((params) => params['id']))
+  );
+
+  public productResource = rxResource({
+    params: () => ({ id: this.productId() }),
+    stream: ({ params }) => {
+      return this.productsService.getProductById(params.id);
+    },
+  });
+
+  public redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigate(['/admin/products']);
+    }
+  });
 }
--- standalone/09-teslo-shop/src/app/dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ standalone/09-teslo-shop/src/app/dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+import { ProductTableComponent } from "../../../products/components/product-table/product-table.component";
+import { ProductService } from '../../../products/services/product.service';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
+import { PaginationService } from '../../../shared/services/pagination.service';
 
 @Component({
   selector: 'dashboard-products-admin-page',
-  imports: [],
+  imports: [PaginationComponent, ProductTableComponent],
   templateUrl: './products-admin-page.component.html',
   styleUrl: './products-admin-page.component.css'
 })
 export class ProductsAdminPageComponent {
+  public productsService = inject(ProductService);
+  public paginationService = inject(PaginationService);
 
+  public productsPerPage = signal(10);
+
+  public productsResource = rxResource({
+    params: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+        limit: params.limit,
+      });
+    },
+  });
 }
