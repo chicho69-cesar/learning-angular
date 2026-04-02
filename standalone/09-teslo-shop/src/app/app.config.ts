a991c1bd255e2821f0d90e635ff98df67e65d40d
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-04-01 19:53:55 -0600

    :sparkles: Finish teslo shop project

--- standalone/09-teslo-shop/src/app/app.config.ts
+++ standalone/09-teslo-shop/src/app/app.config.ts
@@ -1,12 +1,22 @@
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth/interceptors/auth.interceptor';
+import { loggingInterceptor } from './shared/interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes, withHashLocation()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        loggingInterceptor,
+        authInterceptor,
+      ]),
+    ),
+  ],
 };
