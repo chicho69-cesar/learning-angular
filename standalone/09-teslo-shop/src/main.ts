8ace760f5e6fc73e2c5b4f876243a1f9f0cbb9c2
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-04-01 19:48:52 -0600

    :sparkles: Add teslo shop configuration

--- standalone/09-teslo-shop/src/main.ts
+++ standalone/09-teslo-shop/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+
 import { App } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+registerSwiperElements();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
