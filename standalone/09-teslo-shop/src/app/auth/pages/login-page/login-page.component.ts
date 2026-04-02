f8ac1330f009bb36249eefcb11f4b55401bccb18
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2026-04-01 19:53:13 -0600

    :art: Add auth routes

--- standalone/09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ standalone/09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'auth-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  public fb = inject(FormBuilder);
+  public router = inject(Router);
+  public authService = inject(AuthService);
 
+  public hasError = signal(false);
+  public isPosting = signal(false);
+
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  public onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+
+      return;
+    }
+
+    const {
+      email = '',
+      password = '',
+    } = this.loginForm.value;
+
+    this.authService.login(email!, password!).subscribe({
+      next: (isAuthenticated) => {
+        if (isAuthenticated) {
+          this.router.navigateByUrl('/');
+          return;
+        }
+
+        this.hasError.set(true);
+
+        setTimeout(() => {
+          this.hasError.set(false);
+        }, 2000);
+      }
+    });
+  }
 }
--- standalone/09-teslo-shop/src/app/auth/pages/register-page/register-page.component.ts
+++ standalone/09-teslo-shop/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+
+import { FormUtils } from '../../../utils/form.utils';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'auth-register-page',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  public fb = inject(FormBuilder);
+  public router = inject(Router);
+  public authService = inject(AuthService);
 
+  public hasError = signal(false);
+  public isPosting = signal(false);
+
+  public formUtils = FormUtils;
+
+  public registerForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+  }, {
+    validators: [FormUtils.isFieldOneEqualFieldTwo('password', 'confirmPassword')],
+  });
+
+  public onSubmit() {
+    if (this.registerForm.invalid) {
+      this.hasError.set(true);
+
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+
+      return;
+    }
+
+    const {
+      name = '',
+      email = '',
+      password = '',
+    } = this.registerForm.value;
+
+    this.authService.register(name!, email!, password!).subscribe({
+      next: (isAuthenticated) => {
+        if (isAuthenticated) {
+          this.router.navigateByUrl('/');
+          return;
+        }
+
+        this.hasError.set(true);
+
+        setTimeout(() => {
+          this.hasError.set(false);
+        }, 2000);
+      }
+    });
+  }
 }
